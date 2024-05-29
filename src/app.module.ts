fe46e2499a30e1f67a2e59a3a43a7192d3d043c5
hampachovsky <hampe1324@gmail.com>
2024-05-29 10:59:42 +0300

    add connection to cloud db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ClientsModule } from './clients/clients.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TagsModule } from './tags/tags.module';
 import { TasksModule } from './tasks/tasks.module';
+import { TimerIntervalsModule } from './timer-intervals/timer-intervals.module';
 import { TimersModule } from './timers/timers.module';
 import { UserModule } from './user/user.module';
-import { ClientsModule } from './clients/clients.module';
-import { TimerIntervalsModule } from './timer-intervals/timer-intervals.module';
 
 @Module({
   imports: [
@@ -19,11 +19,13 @@ import { TimerIntervalsModule } from './timer-intervals/timer-intervals.module';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('host'),
+        /*  host: configService.get('host'),
         port: +configService.get<number>('DB_PORT'),
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        database: configService.get('DB_NAME'), */
+        url: configService.get('DB_URL'),
+        // autoLoadEntities: true,
         entities: [__dirname + '/**/*.entity{.js, .ts}'],
         synchronize: true,
       }),
